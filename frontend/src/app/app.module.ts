b81e0dcaf0837efb265eb9e1c0bc9e8749e7cfe3
unknown <feel.chanathip@hotmail.com>
2022-08-31 15:06:44 +0700

    [Frontend] - create login page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import {MenubarModule} from 'primeng/menubar';
+import {AccordionModule} from 'primeng/accordion';
+import { NavbarComponent } from './navbar/navbar.component';    
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {CardModule} from 'primeng/card';
+import { FormsModule } from '@angular/forms';
+import {PanelMenuModule} from 'primeng/panelmenu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    MenubarModule,
+    ButtonModule,
+    InputTextModule,
+    CardModule,
+    FormsModule,
+    PanelMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  items!: MenuItem[];
+
+  ngOnInit(): void {
+    this.items = [
+      {
+          label: 'Online Quiz Generator',
+          items: [{
+                  label: 'Proposition list', 
+                  icon: 'pi pi-fw pi-plus',
+              },
+              {label: 'Open'},
+              {label: 'Quit'}
+          ]
+      },
+      {
+          label: 'Edit',
+          icon: 'pi pi-fw pi-pencil',
+          items: [
+              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+          ]
+      }
+  ];
+  }
+
+}
