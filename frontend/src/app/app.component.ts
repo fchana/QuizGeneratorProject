c65f15546cb620e18e08e82921cff0c08ed6ddee
unknown <feel.chanathip@hotmail.com>
2022-10-26 12:45:10 +0700

    calendar fixed, toast fixed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
+  constructor(private messageService: MessageService){
+  
+  }
   title = 'frontend';
 }
